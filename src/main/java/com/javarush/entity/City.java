fe19b6e3dbb564a8df1b8f3e37eb30fac63fed3e
Alina S. Shirokova <shirokova.a.s@mail.ru>
2025-04-07 13:43:47 +0300

    ADD JPA entity mappings for all tables - Create entities - Configure JPA annotations (@OneToMany, @ManyToOne) - Add Lombok annotations

---
+++ src/main/java/com/javarush/entity/City.java
@@ -0,0 +1,34 @@
+package com.javarush.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(schema = "movie", name = "city")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "city_id", columnDefinition = "smallint UNSIGNED not null")
+    private Short id;
+
+    @Column(name = "city", nullable = false, length = 50)
+    private String city;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country;
+
+    @Column(name = "last_update", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime lastUpdate;
+
+    @OneToMany(mappedBy = "city")
+    private Set<Address> addresses;
+}
---
+++ src/main/java/com/javarush/entity/Feature.java
@@ -0,0 +1,31 @@
+package com.javarush.entity;
+
+import lombok.Getter;
+
+import static java.util.Objects.isNull;
+
+@Getter
+public enum Feature {
+    TRAILERS("Trailers"),
+    COMMENTARIES("Commentaries"),
+    DELETED_SCENES("Deleted Scenes"),
+    BEHIND_THE_SCENES("Behind the Scenes");
+
+    private final String value;
+
+    Feature(String value) {
+        this.value = value;
+    }
+
+    public static Feature getFeatureByString(String value) {
+        if (isNull(value) || value.isEmpty()) {
+            return null;
+        }
+        for (Feature feature : Feature.values()) {
+            if (feature.value.equalsIgnoreCase(value)) {
+                return feature;
+            }
+        }
+        return null;
+    }
+}
